3c3d54003274ca59ab2e5517c80e547a8954ec11
Grigoriy Chernish <cga.jsdev@gmail.com>
2023-10-29 12:59:45 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-
+import { RollupCommonJSOptions } from 'vite';
 export default defineConfig({
   define: {
-    global: {},
+    _global: {},
   },
   root: 'src',
   build: {
