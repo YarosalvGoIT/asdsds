525abd229c8ba46e43fa6fce28ce089a4e1d28b0
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-10-29 18:19:34 +0300

    s

--- vite.config.js
+++ vite.config.js
@@ -3,31 +3,19 @@ import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig(({ command }) => {
-  if (command === 'serve') {
-    return {
-      define: {
-        global: {},
-      },
-      root: 'src',
-      build: {
-        rollupOptions: {
-          input: glob.sync('./src/*.html'),
-        },
-        outDir: '../dist',
-      },
-      plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-    };
-  } else {
-    return {
-      root: 'src',
-      build: {
-        rollupOptions: {
-          input: glob.sync('./src/*.html'),
-        },
-        outDir: '../dist',
-      },
-      plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-    };
-  }
+export default defineConfig({
+  define: {
+    global: {},
+  },
+  root: 'src',
+  build: {
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+      entryFileNames: '[commonHelpers]-[hash].js',
+      chunkFileNames: '[commonHelpers]-[hash].js',
+      assetFileNames: '[commonHelpers]-[hash].[ext]',
+    },
+    outDir: '../dist',
+  },
+  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
